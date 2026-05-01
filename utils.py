695a76217118f8470216961530d4cd179016f53c
XinyuGe <131073458+XinyuGee@users.noreply.github.com>
2026-05-01 11:38:55 -0400

    feat(utils): add median calculation function

--- utils.py
+++ utils.py
@@ -12,3 +12,14 @@ def calculate_std(numbers):
     mean = calculate_mean(numbers)
     variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
     return variance ** 0.5
+
+def calculate_median(numbers):
+    """Calculate the median of a list of numbers."""
+    sorted_nums = sorted(numbers)
+    n = len(sorted_nums)
+    if n == 0:
+        return 0
+    mid = n // 2
+    if n % 2 == 0:
+        return (sorted_nums[mid - 1] + sorted_nums[mid]) / 2
+    return sorted_nums[mid]
